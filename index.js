4c53172a26359185a93b5037652d13f5de207bd3
Danilo A. Silva <danilo.official22@hotmail.com>
2023-03-27 17:19:53 -0300

    working a little more with this

--- index.js
+++ index.js
@@ -33,13 +33,57 @@
 //------------------------------------------------------------------------------------//
 
 
-function Pessoa() {
-    this.idade = 0
+// function Pessoa() {
+//     this.idade = 0
 
-    setInterval(function() {
-        this.idade++
-        console.log(this.idade)
-    }, 1000);
+//     setInterval(function() {
+//         this.idade++
+//         console.log(this.idade)
+//     }, 1000);
+// }
+
+// new Pessoa
+
+
+//------------------------------------------------------------------------------------//
+
+
+class Lancamento {
+    constructor(descricao, valor) {
+        this.descricao = descricao
+        this.valor = valor
+    }
 }
 
-new Pessoa
+class CicloFinanceiro {
+    cosntructor(mes, ano){
+        this.mes = mes
+        this.ano = ano
+        this.lancamentos = []
+    }
+
+    addLancamentos(...lancamentos) {
+        let aux_array = []
+        lancamentos.forEach(l => aux_array.push(l))
+    
+        this.lancamentos = aux_array
+    }
+
+    sumario() {
+        let valorTotal = 0
+        this.lancamentos.forEach(l => {
+            valorTotal += l.valor
+        })
+
+        return valorTotal
+    }
+}
+
+const salario = new Lancamento('salario', 4500)
+const contaDeAgua = new Lancamento('conta de agua', -250)
+const contaDeLuz = new Lancamento('conta de lux', -400) // home office ta foda
+const rendimentosFundos = new Lancamento('rendimento dos fundos imobiliarios', 800) 
+
+const ciclo = new CicloFinanceiro('maio', 2021)
+ciclo.addLancamentos(salario, contaDeAgua, contaDeLuz, rendimentosFundos)
+console.log(ciclo.sumario())
