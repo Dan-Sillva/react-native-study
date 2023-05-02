371fb3d738b47ae67b051d8cfc37b7658e12eb0c
Dan-Sillva <danilo.official22@hotmail.com>
2023-05-02 17:24:05 -0300

    creating the course calculator project

---
+++ course-calculator/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>Open up App.js to start working on your app!</Text>
+    </SafeAreaView>
+  );
+}
