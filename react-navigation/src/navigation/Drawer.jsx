33f00bf27bedbee586d85613ad91d5b6e6966997
Dan0Silva <danilo.official22@hotmail.com>
2023-06-21 17:52:16 -0300

    creating drawer navigation

--- react-navigation/src/navigation/Drawer.jsx
+++ react-navigation/src/navigation/Drawer.jsx
@@ -1,4 +1,4 @@
-// import React from "react";
+import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import ViewA from "../views/ViewA";
@@ -9,10 +9,10 @@ const Drawer = createDrawerNavigator()
 
 export default props => {
     return (
-    <Drawer.Navigator>
-        <Drawer.Screen name="View A" component={ViewA} />
-        <Drawer.Screen name="View B" component={ViewB} />
-        <Drawer.Screen name="View C" component={ViewC} />
-    </Drawer.Navigator>
+        <Drawer.Navigator initialRouteName="View A" screenOptions={{headerShown: false}}>
+            <Drawer.Screen name="View A" component={ViewA} />
+            <Drawer.Screen name="View B" component={ViewB} />
+            <Drawer.Screen name="View C" component={ViewC} />
+        </Drawer.Navigator>
     )
 }
